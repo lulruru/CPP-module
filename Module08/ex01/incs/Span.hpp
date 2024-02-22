c28b98888ebbe3e9adff1a79f3dbcbdde4c75397
Russel Encarnacion <russel.enc@gmail.com>
2024-02-22 14:38:06 +0100

    09./01 done

--- Module08/ex01/incs/Span.hpp
+++ Module08/ex01/incs/Span.hpp
@@ -14,24 +14,31 @@
 
 #include <iostream>
 #include <string>
-#include "colors.hpp"
-
+#include <vector>
+#include <list>
+#include <exception>
+#include <algorithm>
 class Span
 {
 	public:
 
 	// Constructeurs & Destructeurs
     Span();
+	Span(unsigned int n);
     Span(Span const &src);
     ~Span();
-
+	void addNumber(int n);
+	void addNumber(std::vector<int> n);
+	int	shortestSpan();
+	int	longestSpan();
 	// Surcharge d'opérateurs
     Span& operator=(Span const &rhs);
 
 	// Getters & Setters
-	
 	// Méthodes
 	
 	private:
+	unsigned int _maxsize;
+	std::vector<int> _cont;
 };
 
--- Module08/ex01/srcs/Span.cpp
+++ Module08/ex01/srcs/Span.cpp
@@ -21,9 +21,15 @@ Span::Span() {
 	std::cout <<  "Default Constructor called: Span"  << std::endl;
 }
 
+Span::Span(unsigned int n) : _maxsize(n) {
+	if(_maxsize > _cont.max_size())
+		throw std::runtime_error("parametre trop gros");
+	std::cout <<  "Default Constructor called: Span"  << std::endl;
+}
+
 
 Span::Span(Span const &src) {
-
+	(void)src;
 	std::cout <<  "Copy Constructor called: Span"  << std::endl;
 }
 
@@ -37,16 +43,48 @@ Span::~Span() {
 /* ************************************************************************** */
 
 Span&	Span::operator=(Span const &rhs) {
-	
+	if(this != &rhs)
+	{
+		_maxsize = rhs._maxsize;
+		_cont = rhs._cont;
+	}
 	std::cout <<  "Assignment Operator called: Span"  << std::endl;
 	return (*this);
 }
 
-/* ************************************************************************** */
-/*                    		 Getters & Setters                  		      */
-/* ************************************************************************** */
+void Span::addNumber(int n){
+	if (_cont.size() == _maxsize)
+		throw std::runtime_error("container est full");
+	_cont.push_back(n);
 
-/* ************************************************************************** */
-/*                   			  Méthodes                 		              */
-/* ************************************************************************** */
+};
 
+void Span::addNumber(std::vector<int> n){
+	if(_cont.size() + n.size() >= _maxsize)
+	n.insert(_cont.end(), n.begin(), n.end());
+};
+
+int	Span::shortestSpan(){
+	if(_cont.size() <= 1)
+		throw std::runtime_error("Pas assez de nombres pour calculer la plus petite distance.");
+
+	std::vector<int> sortedNumbers = _cont;
+	std::sort(sortedNumbers.begin(), sortedNumbers.end());
+	 int shortestDistance = sortedNumbers[1] - sortedNumbers[0];
+	for (size_t i = 2; i < sortedNumbers.size(); ++i) {
+        int currentDistance = sortedNumbers[i] - sortedNumbers[i - 1];
+        if (currentDistance < shortestDistance)
+            shortestDistance = currentDistance;
+    }
+	return shortestDistance;
+};
+
+int	Span::longestSpan(){
+	if(_cont.size() <= 1)
+		throw std::runtime_error("Pas assez de nombres pour calculer la plus longue distance.");
+	std::vector<int> sortedNumbers = _cont;
+	std::sort(sortedNumbers.begin(), sortedNumbers.end());
+	int minElement = sortedNumbers.front();
+    int maxElement = sortedNumbers.back();
+    return maxElement - minElement;
+};
