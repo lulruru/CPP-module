14929fd23f40a3a18b3b9892d935d4f730d3eeee
Russel Encarnacion <russel.enc@gmail.com>
2024-02-22 13:23:59 +0100

    main fixed

--- Module08/ex00/srcs/main.cpp
+++ Module08/ex00/srcs/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rencarna <rencarna@student.42.fr>          +#+  +:+       +#+        */
+/*   By: russelenc <russelenc@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 16:45:18 by rencarna          #+#    #+#             */
-/*   Updated: 2024/02/21 18:23:35 by rencarna         ###   ########.fr       */
+/*   Updated: 2024/02/22 10:44:12 by russelenc        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,18 @@
 
 int	main(int ac, char **av)
 {
-	(void)ac;
-	int	arg = atoi(av[1]);
-	std::list<int> lol;
-	for (int i = 10; i >= 0; i--)
-		lol.push_back(i);
-	try{
-		easyfind(lol, arg);
-	}
-	catch(std::exception &e){
-		
+	if(ac ==2)
+	{
+		int	arg = atoi(av[1]);
+		std::list<int> lol;
+		for (int i = 10; i >= 0; i--)
+			lol.push_back(i);
+		try{
+			easyfind(lol, arg);
+		}
+		catch(std::exception &e){
+			std::cout << arg << " is not found " << std::endl;
+		}
 	}
 }
 
