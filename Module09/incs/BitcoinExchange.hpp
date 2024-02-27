042efef6088bab50d857b5891d0ee0d2575c3eec
Russel Encarnacion <russel.enc@gmail.com>
2024-02-27 13:48:11 +0100

    je sais pas ce que jai modifie je comprend pas

---
+++ Module09/incs/BitcoinExchange.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: russelenc <russelenc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/25 21:43:54 by russelenc          #+#    #+#             */
+/*   Updated: 2024/02/25 21:43:54 by russelenc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class BitcoinExchange
+{
+	public:
+
+	// Constructeurs & Destructeurs
+    BitcoinExchange();
+    BitcoinExchange(BitcoinExchange const &src);
+    ~BitcoinExchange();
+
+	// Surcharge d'opérateurs
+    BitcoinExchange& operator=(BitcoinExchange const &rhs);
+
+	// Getters & Setters
+	
+	// Méthodes
+	
+	private:
+};
+
---
+++ Module09/srcs/BitcoinExchange.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: russelenc <russelenc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/25 21:43:54 by russelenc          #+#    #+#             */
+/*   Updated: 2024/02/25 21:43:54 by russelenc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../incs/BitcoinExchange.hpp"
+
+/* ************************************************************************** */
+/*                     Constructeurs & Destructeurs                   		  */
+/* ************************************************************************** */
+
+BitcoinExchange::BitcoinExchange() {
+
+	std::cout <<  "Default Constructor called: BitcoinExchange"  << std::endl;
+}
+
+
+BitcoinExchange::BitcoinExchange(BitcoinExchange const &src) {
+
+	std::cout <<  "Copy Constructor called: BitcoinExchange"  << std::endl;
+}
+
+BitcoinExchange::~BitcoinExchange() {
+
+	std::cout <<  "Destructor called: BitcoinExchange"  << std::endl;
+}
+
+/* ************************************************************************** */
+/*                     Surcharges d'opérateurs                  		      */
+/* ************************************************************************** */
+
+BitcoinExchange&	BitcoinExchange::operator=(BitcoinExchange const &rhs) {
+	
+	std::cout <<  "Assignment Operator called: BitcoinExchange"  << std::endl;
+	return (*this);
+}
