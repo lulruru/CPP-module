e772c096d8e582a86bfed8545b091361f1ec73b3
Russel ENCARNACION <rencarna@bess-f2r4s2.clusters.42paris.fr>
2024-03-04 14:57:36 +0100

    creation des fichier

---
+++ Module09/ex01/incs/RPN.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rencarna <rencarna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/04 14:57:25 by rencarna          #+#    #+#             */
+/*   Updated: 2024/03/04 14:57:25 by rencarna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class RPN
+{
+	public:
+
+	// Constructeurs & Destructeurs
+    RPN();
+    RPN(RPN const &src);
+    ~RPN();
+
+	// Surcharge d'opérateurs
+    RPN& operator=(RPN const &rhs);
+
+	// Getters & Setters
+	
+	// Méthodes
+	
+	private:
+};
+
