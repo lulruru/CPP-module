a36b6a0239244cd5d2b3eeb2fa795dcf477d366b
Russel ENCARNACION <rencarna@bess-f2r4s2.clusters.42paris.fr>
2024-03-04 15:56:10 +0100

    hey ex01

--- Module09/ex01/srcs/main.cpp
+++ Module09/ex01/srcs/main.cpp
@@ -6,16 +6,56 @@
 /*   By: rencarna <rencarna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 14:57:25 by rencarna          #+#    #+#             */
-/*   Updated: 2024/03/04 14:57:25 by rencarna         ###   ########.fr       */
+/*   Updated: 2024/03/04 15:28:57 by rencarna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/RPN.hpp"
+#include <iostream>
+#include <stack>
+#include <string>
+#include <sstream>
+#include <cctype>
+#include <stdlib.h>
 
-int	main()
-{
+int evaluateRPN(const std::string& expression) {
+    std::istringstream iss(expression);
+    std::stack<int> stack;
+    std::string token;
 
-	return (0);
+    while (iss >> token) {
+        if (isdigit(token[0]) || (token.size() > 1 && isdigit(token[1]))) {
+            stack.push(atoi(token.c_str()));
+        } else {
+            int operand2 = stack.top();
+            stack.pop();
+            int operand1 = stack.top();
+            stack.pop();
+
+            if (token == "+")
+                stack.push(operand1 + operand2);
+            else if (token == "-")
+                stack.push(operand1 - operand2);
+            else if (token == "*")
+                stack.push(operand1 * operand2);
+            else if (token == "/")
+                stack.push(operand1 / operand2);
+        }
+    }
+
+    return stack.top();
 }
 
+int main(int argc, char* argv[]) {
+	std::cout << argv[1][1] << std::endl;
+    if (argc != 2 || !isdigit(argv[1][0]) || argv[1][1] != 32 || argv[1][1]) {
+        std::cerr << "Usage: " << argv[0] << " <expression in RPN>" << std::endl;
+        return 1;
+    }
 
+    std::string expression = argv[1];
+    int result = evaluateRPN(expression);
+    std::cout << "Result: " << result << std::endl;
+
+    return 0;
+}
